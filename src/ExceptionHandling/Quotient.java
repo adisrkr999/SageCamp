0256675a880d00638c7f9c61f68a48824b722dba
adisrkr999 <adi.srkr.oneplus@gmail.com>
2024-02-06 06:08:45 +0530

    frth commit

--- src/ExceptionHandling/Quotient.java
+++ src/ExceptionHandling/Quotient.java
@@ -9,7 +9,7 @@ public class Quotient {
         int number1 = scanner.nextInt();
         int number2 = scanner.nextInt();
         System.out.println(number1+" / "+number2 + " is "+ (number1/number2));
-        int a = 10;
+        int a = 111;
         System.out.println(a);
     }
 }
